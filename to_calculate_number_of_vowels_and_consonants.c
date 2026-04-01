ae90da645a95e04c9dc9f9ff04e815d73a7a6b78
vedam-parthiv-kumar <vedamparthivkumar06@gmail.com>
2026-04-01 23:49:12 +0530

    Create to_calculate_number_of_vowels_and_consonants.c

---
+++ to_calculate_number_of_vowels_and_consonants.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
+int main()
+{
+ char str[MAX_SIZE];
+ int i, len, vowel, consonant;
+ printf("Enter any string: ");
+ fgets(str, sizeof(str), stdin);
+ vowel = 0;
+ consonant = 0;
+ len = strlen(str);
+ for(i=0; i<len; i++)
+ {
+ if((str[i]>='a' && str[i]<='z') || (str[i]>='A' && str[i]<='Z'))
+ {
+ if(str[i] =='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' || 
+ str[i] =='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U' )
+ vowel++;
+ else
+ consonant++;
+ }
+ }
+ printf("Total number of vowel = %d\n", vowel);
+ printf("Total number of consonant = %d\n", consonant);
+ return 0;
+}
